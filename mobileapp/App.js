2905a5a0d7fb0787daaf3f9a075e956322c21687
SanchithaSamarasinghe <139639020+SanchithaSamarasinghe@users.noreply.github.com>
2025-01-06 17:57:35 +0530

    pages+app.js

--- mobileapp/App.js
+++ mobileapp/App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { SafeAreaProvider } from "react-native-safe-area-context";
+import { PaperProvider } from "react-native-paper";
+import Login from "./pages/Login";
+import Home from "./pages/Home";
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
   return (
-    <View style={styles.container}>
-      <Text>hello fucr!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <PaperProvider>
+        <NavigationContainer>
+          <Stack.Navigator
+            // initialRouteName="Home"
+            screenOptions={{
+              headerStyle: { backgroundColor: "#70116d" },
+              headerTitleStyle: { color: "#fff" },
+              headerTitleAlign: "center",
+            }}
+          >
+            <Stack.Screen
+              name="Login"
+              component={Login}
+              options={{ title: "UoV Student Care" }}
+            />
+            <Stack.Screen
+              name="Home"
+              component={Home}
+              options={{ title: "UoV Student Care" }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

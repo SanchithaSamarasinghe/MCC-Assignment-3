233c812319dcb344892c3abfb2945a80f79ff021
SanchithaSamarasinghe <139639020+SanchithaSamarasinghe@users.noreply.github.com>
2025-01-06 17:13:49 +0530

    create components (Banner+Footer)

---
+++ mobileapp/components/Banner.js
@@ -0,0 +1,29 @@
+import { View,Text,Stylesheet, Image }from "react-native";
+import React from "react";
+
+export default function Banner(){
+    return(
+        <View>
+            <Image 
+                source={{
+                    uri:"https://vau.ac.lk/wp-content/uploads/2021/07/cropped-UoV_Logo.png",
+
+                }}
+                style={styles.banner}
+                />
+        </View>        
+
+    
+    );    
+}
+const styles = StyleSheet.create({
+    banner: {
+        width:300,
+        height:100,
+        resizeMode: "contain",
+        alignSelf: "center",
+        marginTop: 20,
+
+    },
+});
+
